1ab58715aa1856f31fffb6eb4c54033a7d837096
Eric Ehlers <eric.ehlers@reposit.be>
2015-08-22 19:33:08 +0200

    implement functions qlCalendarIsEndOfMonth(), qlCalendarEndOfMonth(), qlCalendarAdvance()

--- qlxl/conversions/convert2.cpp
+++ qlxl/conversions/convert2.cpp
@@ -2,11 +2,13 @@
 #include <qlxl/conversions/convert2.hpp>
 #include <qlo/conversions/cnv_tmpl.hpp>
 
-namespace ObjectHandler {
+template<> 
+QuantLib::Date ObjectHandler::convert2<QuantLib::Date, ObjectHandler::ConvertOper>(const ConvertOper& c) {
+    return convertDate(c);
+}
 
-    template<> 
-    QuantLib::Date convert2<QuantLib::Date, ConvertOper>(const ConvertOper& c) {
-        return convertDate(c);
-    }
+template<>
+QuantLib::Period ObjectHandler::convert2<QuantLib::Period, ObjectHandler::ConvertOper>(const ConvertOper& c) {
+    return convertPeriod(c);
 }
 
