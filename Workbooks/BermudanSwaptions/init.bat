649245cbd14de5b9490fcfc36cb51369ab4ebde5
Eric Ehlers <eric.ehlers@reposit.be>
2015-10-19 13:09:44 +0200

    set path to workbook

--- Workbooks/BermudanSwaptions/init.bat
+++ Workbooks/BermudanSwaptions/init.bat
@@ -1,5 +1,6 @@
 
 SET BMD_SWP_XLL_PATH=..\..\xll\QuantLibXL-vc90-mt-s-1_7_0.xll
+@REM SET BMD_SWP_XLL_PATH=..\..\xll\QuantLibXL-vc90-mt-sgd-1_7_0.xll
 SET BMD_SWP_XLS_PATH=.\xlsx\GBP_Market_And_Trades.xlsx
 
 @REM read write
