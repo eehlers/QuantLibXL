1259e11fae5f1d008d2e10203746337e742f17c3
Eric Ehlers <eric.ehlers@nazcatech.be>
2015-12-05 20:47:12 +0200

    increment version number

--- make_zip.py
+++ make_zip.py
@@ -9,8 +9,8 @@ import argparse
 import re
 
 QLXL = "QuantLibXL"
-VERSION = "1.7.0"
-VERSION_ = "1_7_0"
+VERSION = "1.8.0"
+VERSION_ = "1_8_0"
 #VC_VERSION = "vc120"
 VC_VERSION = "vc90"
 QLXL_VERSION = QLXL + "-" + VERSION
