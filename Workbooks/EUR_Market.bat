0271609fdd25d2cfe872e6ac8e4f44c1333aa061
Eric Ehlers <eric.ehlers@reposit.be>
2015-12-20 20:08:31 +0100

    eur market workbook

---
+++ Workbooks/EUR_Market.bat
@@ -0,0 +1,7 @@
+
+SET XLL_PATH=.\utils\QuantLibXL-vc120-mt-s-1_7_0.xll
+@REM 30-SEP-2015
+SET EVAL_DATE=42277
+SET XLS_PATH=.\EUR_Market\EUR_Market.xlsm
+START "EXCEL" "EXCEL.EXE" /e utils\utils.xlsm .\init.xlam
+
