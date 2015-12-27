59e4cf450f2f9bdb8f80f8b8a33cb293b272d912
Eric Ehlers <eric.ehlers@reposit.be>
2015-12-27 15:18:58 +0100

    update batch file

--- Workbooks/EUR_Market.bat
+++ Workbooks/EUR_Market.bat
@@ -1,7 +1,7 @@
 
-SET XLL_PATH=.\utils\QuantLibXL-vc120-mt-s-1_7_0.xll
+SET XLL_PATH=..\xll\QuantLibXL-vc90-mt-s-1_8_0.xll
+SET XLS_PATH=.\EUR_Market\EUR_Market.xlsm
 @REM 30-SEP-2015
 SET EVAL_DATE=42277
-SET XLS_PATH=.\EUR_Market\EUR_Market.xlsm
 START "EXCEL" "EXCEL.EXE" /e .\init.xlam
 
