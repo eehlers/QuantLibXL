d8a12add8575ac74f991a05b57278c60747dfe83
Eric Ehlers <eric.ehlers@reposit.be>
2016-01-13 06:32:33 +0100

    add bat file for 64-bit build

---
+++ Workbooks/EUR_Market-x64.bat
@@ -0,0 +1,7 @@
+
+SET XLL_PATH=..\xll\QuantLibXL-vc90-x64-mt-s-1_8_0.xll
+SET XLS_PATH=.\EUR_Market\EUR_Market.xlsm
+@REM 30-SEP-2015
+SET EVAL_DATE=42277
+START "EXCEL" "EXCEL.EXE" /e .\init.xlam
+
