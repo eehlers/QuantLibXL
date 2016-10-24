03fb033288a8840d99dd51fb3039c02bf0d5635e
Eric Ehlers <eric.ehlers@reposit.be>
2016-10-24 22:08:39 +0200

    a unit test framework

---
+++ UnitTests/UnitTests.bat
@@ -0,0 +1,6 @@
+
+SET XLL_OLD_PATH=..\..\..\QuantLibAddin-Old\QuantLibXL\xll\QuantLibXL-vc90-mt-s-1_8_0.xll
+SET XLL_NEW_PATH=..\xll\QuantLibXL-vc90-mt-s-1_8_0.xll
+@REM SET XLS_PATH=.\UnitTests.xlsx
+START "EXCEL" "EXCEL.EXE" /e UnitTests.xlam
+
