908826d4b30c12c906db220d2ec0c879379caa6d
Eric Ehlers <eric.ehlers@reposit.be>
2018-02-10 12:58:14 +0100

    upgrade QuantLib from 1.11 to 1.12

--- UnitTests/UnitTests.old.bat
+++ UnitTests/UnitTests.old.bat
@@ -1,5 +1,5 @@
 
-SET XLL_PATH=..\..\..\QuantLibAddin-Old\QuantLibXL\xll\QuantLibXL-vc141-mt-s-1_11_0.xll
+SET XLL_PATH=..\..\..\QuantLibAddin-Old\QuantLibXL\xll\QuantLibXL-vc141-mt-s-1_12_0.xll
 :: 01-OCT-2016
 SET EVAL_DATE=42644
 START "EXCEL" "EXCEL.EXE" /e UnitTests.xlam
